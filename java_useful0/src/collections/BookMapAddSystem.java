8f2f3ad178274d1d80959be1291e0de5388ec37b
Yeram Lee <coramdeo643@kakao.com>
2025-05-07 15:03:38 +0900

    250507.1500

--- java_useful0/src/collections/BookMapAddSystem.java
+++ java_useful0/src/collections/BookMapAddSystem.java
@@ -7,28 +7,40 @@ public class BookMapAddSystem {
 
     public BookMapAddSystem() {
         bookHashMap = new HashMap<>();
+        //bookHashMap.put("Philosopher's Stone", new Book("Philosopher's Stone", "J.K.Rowling", 2001));
     }
 
     // 저장하는 메서드
-    public void addBooks(String title, String author) {
-        bookHashMap.put(title, new Book(title, author));
+    public void addBooks(String title, String author, int year) {
+        bookHashMap.put(title, new Book(title, author, year));
+
     }
 
     // 출력하는 메서드(책 제목 + 저자)
     public void printBooks() {
+        System.out.println("List of all the books : ");
         for (Book b : bookHashMap.values()) {
             System.out.println(b.toString());
         }
     }
 
-    public static void main(String[] args) {
-        BookMapAddSystem BMAS = new BookMapAddSystem();
-        BMAS.addBooks("Philosopher's Stone", "J.K.Rowling");
-        BMAS.addBooks("Chamber of Secret", "J.K.Rowling");
-        BMAS.addBooks("Prisoner of Azkaban", "J.K.Rowling");
-        BMAS.addBooks("the Goblet of Fire", "J.K.Rowling");
+    public void countBooks() {
+        System.out.println("Total numbers of the books : " + bookHashMap.size());
+    }
 
-        BMAS.printBooks();
+    public static void main(String[] args) {
+        BookMapAddSystem bookMAS = new BookMapAddSystem();
+        bookMAS.addBooks("Philosopher's Stone", "J.K.Rowling", 2001);
+        bookMAS.addBooks("Chamber of Secret", "J.K.Rowling", 2002);
+        bookMAS.addBooks("Prisoner of Azkaban", "J.K.Rowling", 2004);
+        bookMAS.addBooks("Goblet of Fire", "J.K.Rowling", 2005);
+        bookMAS.addBooks("Order of the Phoenix", "J.K.Rowling", 2007);
+        bookMAS.addBooks("Half-Blood Prince", "J.K.Rowling", 2009);
+        bookMAS.addBooks("Deathly Hallows", "J.K.Rowling", 2010);
+
+        bookMAS.printBooks();
+        bookMAS.countBooks();
+
     }
 }
 
@@ -36,17 +48,21 @@ public class BookMapAddSystem {
 class Book {
     String title;
     String author;
+    int year;
+    int view_count;
 
-    public Book(String title, String author) {
+    public Book(String title, String author, int year) {
         this.title = title;
         this.author = author;
+        this.year = year;
     }
 
     @Override
     public String toString() {
-        return "Book { " +
-                "title : '" + title + '\'' +
-                ", author : '" + author + '\'' +
-                " } ";
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                '}';
     }
 }
