1d72233839b7ce4db920523b8f68d341b332124f
Yeram Lee <coramdeo643@kakao.com>
2025-05-07 14:02:26 +0900

    250507.1400

---
+++ java_swing/src/bubble/test06/Bubble.java
@@ -0,0 +1,161 @@
+package bubble.test05;
+
+import javax.swing.*;
+
+public class Bubble extends JLabel implements Moveable {
+
+    private int x;
+    private int y;
+    // bubble moving
+    private boolean left;
+    private boolean right;
+    private boolean up;
+
+    private boolean isL;
+    private boolean isR;
+
+
+    private boolean lftWallTouch;
+    private boolean rtWallTouch;
+
+    private ImageIcon bubble; // BASIC BUBBLE
+
+    private Player player;
+
+    @Override
+    public int getX() {
+        return x;
+    }
+    @Override
+    public int getY() {
+        return y;
+    }
+    public boolean isLeft() {
+        return left;
+    }
+    public boolean isRight() {
+        return right;
+    }
+    public boolean isUp() {
+        return up;
+    }
+    public boolean isLftWallTouch() {
+        return lftWallTouch;
+    }
+    public boolean isRtWallTouch() {
+        return rtWallTouch;
+    }
+    public ImageIcon getBubble() {
+        return bubble;
+    }
+    public Player getPlayer() {
+        return player;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    public void setLeft(boolean left) {
+        this.left = left;
+    }
+    public void setRight(boolean right) {
+        this.right = right;
+    }
+    public void setUp(boolean up) {
+        this.up = up;
+    }
+    public void setLftWallTouch(boolean lftWallTouch) {
+        this.lftWallTouch = lftWallTouch;
+    }
+    public void setRtWallTouch(boolean rtWallTouch) {
+        this.rtWallTouch = rtWallTouch;
+    }
+    public void setBubble(ImageIcon bubble) {
+        this.bubble = bubble;
+    }
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    // 생성자를 통해서 Player 객체의 주소값을 주입받기 -> 생성자 의존 주입
+    public Bubble (Player player) {
+        this.player = player;
+        init();
+        setInitLO();
+        // bubble is enough for one thread
+        bubbleStartThread();
+    }
+
+    private void bubbleStartThread() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if(player.getPlayerWay() == PlayerWay.LEFT) {
+                    left();
+                } else {
+                    right();
+                }
+            }
+        }).start();
+    }
+
+    private void init(){
+        bubble = new ImageIcon("img/bubble.png");
+        left = false;
+        right = false;
+        up = false;
+    }
+    private void setInitLO(){
+        x=player.getX(); // get player location
+        y=player.getY();
+        setIcon(bubble);
+        setSize(50,50);
+        setLocation(x,y);
+    }
+
+    @Override
+    public void left() {
+        left = true;
+        for (int i = 0; i < 400; i++) {
+            x--;
+            setLocation(x,y);
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } // t-c
+        }// for
+        up();
+    }
+
+    @Override
+    public void right() {
+        right = true;
+        for (int i = 0; i < 400; i++) {
+            x++;
+            setLocation(x,y);
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } // t-c
+        }// for
+        up();
+    }
+
+    @Override
+    public void up() {
+        up=true;
+        while(true) {
+            y--;
+            setLocation(x,y);
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } // t-c
+        }
+    }
+} // end of class
---
+++ java_swing/src/bubble/test06/Player.java
@@ -0,0 +1,252 @@
+package bubble.test05;
+
+import javax.swing.*;
+
+public class Player extends JLabel implements Moveable {
+
+    private int x;
+    private int y;
+
+    private ImageIcon playerR;
+    private ImageIcon playerRDie;
+    private ImageIcon playerL;
+    private ImageIcon playerLDie;
+
+    // player speed status
+    private final int SPEED = 4;
+    private final int JUMP_SPEED = 2;
+
+    // player moving status
+    private boolean left;
+    private boolean right;
+    private boolean up;
+    private boolean down;
+
+    // wall touch status
+    private boolean lWallTouch;
+    private boolean rWallTouch;
+
+    // player direction status(how to use enum 1 - declare)
+    private PlayerWay playerWay;
+    // PlayerWay - construct getter only
+    public PlayerWay getPlayerWay() {
+        return playerWay;
+    }
+
+    // Alt+Insert Getter Setter
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    public ImageIcon getPlayerR() {
+        return playerR;
+    }
+
+    public ImageIcon getPlayerRDie() {
+        return playerRDie;
+    }
+
+    public ImageIcon getPlayerL() {
+        return playerL;
+    }
+
+    public ImageIcon getPlayerLDie() {
+        return playerLDie;
+    }
+
+    public int getSPEED() {
+        return SPEED;
+    }
+
+    public int getJUMP_SPEED() {
+        return JUMP_SPEED;
+    }
+
+    public boolean isLeft() {
+        return left;
+    }
+
+    public boolean isRight() {
+        return right;
+    }
+
+    public boolean isUp() {
+        return up;
+    }
+
+    public boolean isDown() {
+        return down;
+    }
+
+    public boolean islWallTouch() {
+        return lWallTouch;
+    }
+
+    public boolean isrWallTouch() {
+        return rWallTouch;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setPlayerR(ImageIcon playerR) {
+        this.playerR = playerR;
+    }
+
+    public void setPlayerRDie(ImageIcon playerRDie) {
+        this.playerRDie = playerRDie;
+    }
+
+    public void setPlayerL(ImageIcon playerL) {
+        this.playerL = playerL;
+    }
+
+    public void setPlayerLDie(ImageIcon playerLDie) {
+        this.playerLDie = playerLDie;
+    }
+
+    public void setLeft(boolean left) {
+        this.left = left;
+    }
+
+    public void setRight(boolean right) {
+        this.right = right;
+    }
+
+    public void setUp(boolean up) {
+        this.up = up;
+    }
+
+    public void setDown(boolean down) {
+        this.down = down;
+    }
+
+    public void setlWallTouch(boolean lWallTouch) {
+        this.lWallTouch = lWallTouch;
+    }
+
+    public void setrWallTouch(boolean rWallTouch) {
+        this.rWallTouch = rWallTouch;
+    }
+
+    public Player() {
+        initData();
+        setInitLayout();
+    }
+
+    private void initData() {
+        playerR = new ImageIcon("img/playerR.png");
+        playerRDie = new ImageIcon("img/playerRDie.png");
+        playerL = new ImageIcon("img/playerL.png");
+        playerLDie = new ImageIcon("img/playerLDie.png");
+        // player initial location status
+        x = 55;
+        y = 535;
+        left = false;
+        right = false;
+        up = false;
+        down = false;
+    }
+
+    private void setInitLayout() {
+        setSize(50, 50);
+        setIcon(playerR);
+        setLocation(x, y);
+    }
+
+    @Override
+    public void left() {
+        // 클래스 이름으로 접근
+        playerWay = PlayerWay.LEFT;
+        left = true;
+        setIcon(playerL);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (left) {
+                    setLocation(x -= SPEED, y);
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                } // while
+            } // run()
+        }).start();
+
+    }
+
+    @Override
+    public void right() {
+        playerWay = PlayerWay.RIGHT;
+        right = true; // moving status modified
+        setIcon(playerR);
+        // anonymous class - thread.start() - run() 구문 동작
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (right) {
+                    setLocation(x += SPEED, y);
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }).start(); // 객체 생성후 바로 start
+        //System.out.println("player right() method call");
+        //setLocation(x+=10,y);
+    }
+
+    @Override
+    public void up() {
+        up = true;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < (130 / JUMP_SPEED); i++) {
+                    setLocation(x, y -= JUMP_SPEED);
+                    try {
+                        Thread.sleep(5);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                } // for
+                up = false;
+                down();
+            } // run()
+        }).start();
+    }
+
+    @Override
+    public void down() {
+        down = true;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < (130 / JUMP_SPEED); i++) {
+                    setLocation(x, y += JUMP_SPEED);
+                    try {
+                        Thread.sleep(4);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                down = false;
+            }
+        }).start();
+
+    }
+}
