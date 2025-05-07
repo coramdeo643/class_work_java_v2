95535ba7eee715530fab944ff98037814a08e4f0
Yeram Lee <coramdeo643@kakao.com>
2025-05-07 10:43:43 +0900

    250507.1040

--- java_useful0/src/collections/MySetTest.java
+++ java_useful0/src/collections/MySetTest.java
@@ -1,4 +1,69 @@
 package collections;
 
+import java.util.*;
+
 public class MySetTest {
-}
+
+    public static void main(String[] args) {
+
+        // 계층 구조 확인 (업캐스팅된 상태)
+        Set<Integer> mTestSet = new HashSet<>();
+
+        // 사용방법
+
+        // HashSet : 중복된 요소를 허용하지 않고, 순서가 없는 자료 구조
+        HashSet<Integer> numberSet = new HashSet<>();
+
+        // 요소 추가하기
+        numberSet.add(500);
+        numberSet.add(11);
+        numberSet.add(200);
+        numberSet.add(333);
+        numberSet.add(553);
+        System.out.println("numberSet size : " + numberSet.size());
+        System.out.println("numberSet = " + numberSet);
+
+        System.out.println("- - - - - - - - - - - - - - - - - - - - - ");
+        // 요소 제거
+        numberSet.remove(1); // remove, 없는 obj 선택해도 오류발생 X
+
+        System.out.println("numberSet size : " + numberSet.size());
+        System.out.println("- - - - - - - - - - - - - - - - - - - - - ");
+        // 자료구조는 반복문과 함께 많이 사용됨
+        // for (int i = 0; i < 10; i++) { } - 인덱스 for 구문 사용 불가!
+        // enhanced for
+        for (Integer num : numberSet) {
+            System.out.println("num 값 확인 : " + num);
+        }
+        System.out.println("- - - - - - - - - - - - - - - - - - - - - ");
+        // Set 계열을 Iterator 타입으로 변환해서 순회가능
+        Iterator<Integer> iter = numberSet.iterator(); // 형변환처리 Set --> Iterator Conversion
+        while (iter.hasNext()) {
+            System.out.println("값 : " + iter.next()); // next() 요소를 반환처리한다
+        }
+
+        System.out.println("- - - - - - - - - - - - - - - - - - - - - ");
+        // Set 계열 자료 구조 선언(정수값만 담을수 있고 중복 허용 불가)
+        HashSet<Integer> lottoNumbers = new HashSet<>();
+        while (lottoNumbers.size() < 6) {
+            lottoNumbers.add(getRandomNumber());
+        }
+        System.out.println("Random Lotto Numbers : " + lottoNumbers.toString());
+
+        // HashSet DT --> ArrayList 객체 생성시,
+        // 즉 생성자에 인자값으로 넣을수 있도록 설계되어있음
+        ArrayList<Integer> sortedList = new ArrayList<>(lottoNumbers);
+        // 정렬하기 기능 사용해보기
+        Collections.sort(sortedList);
+        System.out.println("Sorted Lotto Numbers : " + sortedList);
+
+    } // end of main
+
+    // 무작위 1부터 45까지 랜덤 번호 생성 함수
+    public static int getRandomNumber() {
+        Random random = new Random();
+        return random.nextInt(45) + 1;
+    }
+
+
+} // end of class
--- java_useful0/src/collections/UniqueUserIDSystem.java
+++ java_useful0/src/collections/UniqueUserIDSystem.java
@@ -1,4 +1,87 @@
 package collections;
 
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class UniqueUserIDSystem {
-}
+
+//### **예제 개념: 고유 사용자 ID 관리 시스템**
+//**상황 설명**: 웹사이트에 접속하는 사용자의 ID를 관리하는 시스템을 개발합니다.
+// 사용자 ID는 고유해야 하며 중복을 허용하지 않습니다.
+// 시스템은 사용자가 로그인할 때마다 ID를 체크하고, 새로운 사용자 ID를 등록할 때 중복을 방지합니다.
+    // 상황 : 사용자가 키보드로 입력한 ID 데이터를 많이 저장해야함
+    // 고유한 값이어야함. 메모리상으로 관리할 예정
+    private HashSet<String> registeredIDs;
+    public UniqueUserIDSystem() {
+        registeredIDs = new HashSet<>();
+        // printAllIDs method test, sample data prepare
+        // TODO sample data preparation, will be deleted
+        registeredIDs.add("Hong");
+        registeredIDs.add("Teemo");
+        registeredIDs.add("Lee");
+    }
+
+    // 사용자 ID를 등록하는 메서드
+    public boolean addUserID(String userID) {
+        return registeredIDs.add(userID);
+    }
+
+    // 사용자 ID를 출력하는 메서드 직접 만들어보자 - 전체출력
+    // 메서드 이름은 동사로 시작하는것이 권장사항
+    public void printAllUserID () { // 1. 함수 이름 고민하기
+        // System.out.println("List of all IDs : " + registeredIDs.toString());
+        // registeredIDs 순회해서 콘솔창에 출력하고자한다
+        // 초기화 for, Iterator 형변환해서 순회가능
+        // HashSet 안에 요소가 아무것도 없다면
+        if (registeredIDs.isEmpty()) {
+            System.out.println("There is no UserID");
+            return; // 실행의 제어권 반납
+        }
+        for(String id : registeredIDs) {
+            System.out.println("id : " + id);
+        }
+        System.out.println("총 등록된 id 개수 : " + registeredIDs.size());
+    }
+
+    // 시스템을 실행할 메서드
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("사용하고싶은 ID를 입력하세요(종료하려면 'exit' 입력 / ID 목록 보기 = 'list' 입력) : ");
+            String input = scanner.nextLine().trim(); // .trim() 공백 제거
+
+            // 사용자가 키보드에서 값을 넣지않고 바로 enter 누르는 경우
+            // 빈 입력 처리
+            if(input.isEmpty()) {
+                System.out.println("ID 를 입력해주세요");
+                continue; // 스킵하고 다시 진행
+            }
+
+            if ("list".equals(input)) {
+                printAllUserID();
+                continue;
+            }
+            if("exit".equals(input)) {
+                System.out.println("System exit");
+                break;
+            } // end of if
+
+            boolean isRegistered = addUserID(input);
+            if(isRegistered) {
+                System.out.println("새로운 사용자 ID 가 등록되었습니다");
+            } else {
+                System.out.println("이미 등록된 사용자 ID 입니다");
+            }
+
+        } // end of while
+        scanner.close(); // 자원해제
+    } // end of start()
+
+    // main
+    public static void main(String[] args) {
+        UniqueUserIDSystem idSystem = new UniqueUserIDSystem();
+        idSystem.start();
+        // idSystem.printAllUserID();
+
+    } // end of main
+} // end of class
