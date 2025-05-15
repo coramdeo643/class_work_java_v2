9604d622559f5a97c650a7a2b517bc3cc694e399
Yeram Lee <coramdeo643@kakao.com>
2025-05-15 11:46:54 +0900

    250515.JDBC.CafeMenuManager.02

--- java_jdbc/src/cho04/CafeMenuManager.java
+++ java_jdbc/src/cho04/CafeMenuManager.java
@@ -25,15 +25,82 @@ public class CafeMenuManager {
             pstmt.setString(3, category);
             int rowsInsertedCount = pstmt.executeUpdate(); // 쿼리 실행(Insert, update, delete)
             System.out.println(rowsInsertedCount + " 개 메뉴 추가 성공!");
-
         } catch (Exception e) {
             System.err.println("Error inserting menu" + e.getMessage());
             e.printStackTrace();
         }
     }
 
+    // Price update method
+    // update menu set price = 3000 where name = "Today's coffee";
+    public static void updateMenuPrice(int price, String name) {
+        try (Connection connection = DriverManager.getConnection(URL,USER,PWD)) {
+            String sql = "update menu set price = ? where name = ? ";
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setInt(1, price);
+            pstmt.setString(2, name);
+            int rowsUpdatedCount = pstmt.executeUpdate(); // 쿼리 실행(Insert, update, delete)
+            System.out.println(rowsUpdatedCount + " 개 메뉴 가격 수정 성공!");
+        } catch (Exception e) {
+            System.err.println("Error updating price" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    // Update menu name
+    public static void updateMenuName(String changedName, String name) {
+        try (Connection connection = DriverManager.getConnection(URL,USER,PWD)) {
+            String sql = "update menu set name = ? where name = ? ";
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, changedName);
+            pstmt.setString(2, name);
+            int rowsUpdatedCount = pstmt.executeUpdate(); // 쿼리 실행(Insert, update, delete)
+            System.out.println(rowsUpdatedCount + " 개 메뉴 이름 수정 성공!");
+        } catch (Exception e) {
+            System.err.println("Error updating price" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    // delete menu
+    public static void deleteMenu(String name) {
+        try (Connection connection = DriverManager.getConnection(URL,USER,PWD)) {
+            String sql = "delete from menu where name = ? ";
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, name);
+            int rowsUpdatedCount = pstmt.executeUpdate(); // 쿼리 실행(Insert, update, delete)
+            System.out.println(rowsUpdatedCount + " 개 메뉴 삭제 성공!");
+        } catch (Exception e) {
+            System.err.println("Error deleting price" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    // select all menu
+    public static void selectAllMenu() {
+        // 반드시 쿼리 확인하고 복사붙여넣기 하기
+        // >> 그리고 메서드 파라미터 결정
+        // >> 필요 코드 구현 및 테스트
+        try (Connection connection = DriverManager.getConnection(URL,USER,PWD)) {
+            String sql = "select * from menu ";
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                System.out.println("| name : " + rs.getString("name")
+                        + " | price : " + rs.getInt("price")
+                        + " | category : " + rs.getString("category") + " |");
+            }
+            System.out.println("전 메뉴 조회 성공!");
+        } catch (Exception e) {
+            System.err.println("Error selecting all menu" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         // 테스트 해보기
-        insertMenu("Today's coffee" , 2000, "Coffee");
+        // insertMenu("Espresso" , 1000, "Coffee");
+        // updateMenuPrice(3000, "Today's coffee");
+        // updateMenuName("Brewed coffee", "Today's coffee");
+        // deleteMenu("Today's coffee");
+        selectAllMenu();
     }
 }
